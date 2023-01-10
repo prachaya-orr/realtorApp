978a9b40f84a49dc2b45d719d57b25c8aaf94c39
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-01-10 21:36:21 +0700

    54. Storing the User in the Database

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
 
 interface SignupParams {
   email: string;
@@ -13,7 +14,7 @@ interface SignupParams {
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async signup({ email, password }: SignupParams) {
+  async signup({ email, password, name, phone }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: { email },
     });
@@ -24,6 +25,16 @@ export class AuthService {
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    console.log({ hashedPassword  });
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return user;
   }
 }
