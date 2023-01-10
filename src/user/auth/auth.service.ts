59fd88af2ae77715cd73e7df231c6c2fd0a15287
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-01-10 20:28:20 +0700

    53. Hashing Our Password

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 interface SignupParams {
   email: string;
@@ -12,7 +13,7 @@ interface SignupParams {
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async signup({ email }: SignupParams) {
+  async signup({ email, password }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: { email },
     });
@@ -20,5 +21,9 @@ export class AuthService {
     if (userExists) {
       throw new ConflictException();
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    console.log({ hashedPassword  });
   }
 }
