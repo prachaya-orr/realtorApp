96c868fc91b5f48266a1dc0ed8639f42151dad6a
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-01-12 23:18:11 +0700

    79. Pseudo-Coding Our Guard Logic

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -2,8 +2,9 @@ import { CanActivate } from '@nestjs/common';
 
 export class AuthGuard implements CanActivate {
   canActivate() {
+    // 1) Determine the UserTypes that can execute the called end point
+    // 2) Gran the JWT from the request header and verify it
 
-    
     return false;
   }
 }
