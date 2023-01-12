2d2c83224aaad911b0d124a8c3153384b4d15866
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-01-12 23:18:55 +0700

    fix:79. Pseudo-Coding Our Guard Logic

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -4,7 +4,8 @@ export class AuthGuard implements CanActivate {
   canActivate() {
     // 1) Determine the UserTypes that can execute the called end point
     // 2) Gran the JWT from the request header and verify it
-
+    // 3) Database request to get user by id
+    // 4) Determine if the user can permission
     return false;
   }
 }
