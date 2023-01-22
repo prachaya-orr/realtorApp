5906acd9414074bc500922a3d1aa8ff6355f424d
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-01-22 16:28:00 +0700

    96. More Controller Tests

--- src/home/home.controller.spec.ts
+++ src/home/home.controller.spec.ts
@@ -1,7 +1,28 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PropertyType } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
+
+const mockUser = {
+  id: 53,
+  name: 'ohm',
+  email: 'ohm@buyer.ohm',
+  phone: '555 555 5555',
+};
+
+const mockHome = {
+  id: 1,
+  address: '2345 William Str',
+  city: 'Toronto',
+  price: 1500000,
+  propertyType: PropertyType.RESIDENTIAL,
+  image: 'src0',
+  numberOfBedrooms: 3,
+  numberOfBathrooms: 2.5,
+  landSize: 20,
+};
 
 describe('HomeController', () => {
   let controller: HomeController;
@@ -15,6 +36,8 @@ describe('HomeController', () => {
           provide: HomeService,
           useValue: {
             getHomes: jest.fn().mockReturnValue([]),
+            getRealtorByHomeId: jest.fn().mockReturnValue(mockUser),
+            updateHomeById: jest.fn().mockReturnValue(mockHome),
           },
         },
         PrismaService,
@@ -37,6 +60,30 @@ describe('HomeController', () => {
           gte: 1500000,
         },
       });
+    });
+  });
+
+  describe('updateHome', () => {
+    const mockUserInfo = {
+      name: 'ohm',
+      id: 30,
+      iat: 1,
+      exp: 3,
+    };
+    const mockCreateHomeParams = {
+      address: '11 yellow street',
+      numberOfBathrooms: 2,
+      numberOfBedrooms: 2,
+      city: 'Vancouver',
+      landSize: 4444,
+      price: 3000000,
+      propertyType: PropertyType.RESIDENTIAL,
+    };
+
+    it("should throw unauth error if realtor didn't create home", async () => {
+      await expect(
+        controller.updateHome(5, mockCreateHomeParams, mockUserInfo),
+      ).rejects.toThrowError(UnauthorizedException);
     });
   });
 });
